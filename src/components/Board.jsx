92d22fbcb1342cc481f1cf5f3b45bb65fe14d1ab
Joaquin Nietes <jnietes04@gmail.com>
2025-08-13 13:32:59 -0400

    Stored board as matrix first, then render

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,12 +3,18 @@ import Tile from "./Tile";
 function Board() {
   const size = 13;
 
+  // Initializing empty board: 0: empty; 1: white; -1: black;
+  const board = Array(size)
+    .fill(0)
+    .map(() => Array(size).fill(0));
+  console.log(board);
+
   return (
     <>
-      {Array.from({ length: size }, (_) => (
-        <div class="rowOfTiles">
-          {Array.from({ length: size }, (_, index) => (
-            <Tile key={index} />
+      {board.map((row, rowIndex) => (
+        <div key={rowIndex} className="newRow">
+          {row.map((_, colIndex) => (
+              <Tile key={(rowIndex + ',' + colIndex)} />
           ))}
         </div>
       ))}
