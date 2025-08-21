7cbd7eb15efd1fffd40c4f8ccb7d6756eb431b46
Joaquin Nietes <jnietes04@gmail.com>
2025-08-20 23:24:20 -0400

    Implemented win condition

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,8 +2,16 @@ import { useState } from "react";
 import Tile from "./Tile";
 
 function Board() {
-  const size = 13;
+  const [size, setSize] = useState(13);
   const [currentTurn, setCurrentTurn] = useState(-1);
+
+  // Represents all pieces withing a 4 tile star radius.
+  const [winMat, setWinMat] = useState([
+    [[-4, -4], [-3, -3], [-2, -2], [-1, -1], [1, 1], [2, 2], [3, 3], [4, 4]], // \
+    [[-4, 4], [-3, 3], [-2, 2], [-1, 1], [1, -1], [2, -2], [3, -3], [4, -4]],  // /
+    [[-4, 0], [-3, 0], [-2, 0], [-1, 0], [1, 0], [2, 0], [3, 0], [4, 0]],     // |
+    [[0, -4], [0, -3], [0, -2], [0, -1], [0, 1], [0, 2], [0, 3], [0, 4]]]     // -
+  );
 
   // Stores the stones placed on the board.
   // 0: none; -1: black; 1: white;
@@ -20,12 +28,14 @@ function Board() {
           {row.map((_, colIndex) => (
             <Tile
               key={rowIndex + "," + colIndex}
+              size={size}
               index={rowIndex + "," + colIndex}
               element={matrix[rowIndex][colIndex]}
               matrix={matrix}
               setMatrix={setMatrix}
               currentTurn={currentTurn}
               setCurrentTurn={setCurrentTurn}
+              winMat={winMat}
             />
           ))}
         </div>
--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,24 +1,31 @@
-import { useState } from "react";
-
 function Tile({
   index,
+  size,
   element,
   matrix,
   setMatrix,
   currentTurn,
   setCurrentTurn,
+  winMat,
 }) {
   const row = index.split(",")[0];
   const col = index.split(",")[1];
 
   let colorClass = "";
+  let currentColor = "";
 
   if (currentTurn == -1) {
     colorClass = "blackTurn";
+    currentColor = "Black";
   }
-  
+
   if (currentTurn == 1) {
     colorClass = "whiteTurn";
+    currentColor = "White";
+  }
+
+  if (currentTurn == 0) {
+    colorClass = "deadTile"; // Dead, as in not animated
   }
 
   if (element == -1) {
@@ -29,20 +36,61 @@ function Tile({
     colorClass = "whiteStone";
   }
 
-  function handleClick() {
-
-    if (matrix[row][col] == 0) {
-      let copy = Array.from(matrix);
-      if (currentTurn == -1) {
-        copy[row][col] = -1;
-      } else {
-        copy[row][col] = 1;
-      }
-      setMatrix(copy);
+  function newIndexInside(index, delta, size) {
+    let inside = false;
+    let newIndex = parseInt(index) + parseInt(delta);
+    if (newIndex > 0 && newIndex < size) {
+      inside = true;
     }
+    return inside;
+  }
 
-    // -1: black; 1: white;
-    setCurrentTurn(-currentTurn);
+  function handleClick() {
+    if (currentTurn != 0) {
+      if (matrix[row][col] == 0) {
+        let copy = Array.from(matrix);
+        if (currentTurn == -1) {
+          copy[row][col] = -1;
+        } else {
+          copy[row][col] = 1;
+        }
+        setMatrix(copy);
+      }
+
+      // Detect matching stones in 4 star raduis.
+      let matchingStones = 0;
+      for (let i = 0; i < winMat.length; i++) {
+        matchingStones = 0;
+        for (let j = 0; j < winMat[0].length; j++) {
+          let rowDelta = winMat[i][j][0];
+          let colDelta = winMat[i][j][1];
+          let rowPlusDelta = parseInt(row) + parseInt(rowDelta);
+          let colPlusDelta = parseInt(col) + parseInt(colDelta);
+
+          if (
+            newIndexInside(row, rowDelta, size) &&
+            newIndexInside(col, colDelta, size)
+          ) {
+            if (matrix[rowPlusDelta][colPlusDelta] == currentTurn) {
+              matchingStones++;
+            }
+
+            // Checks #/8 contiguous tiles then sets count 0
+            else {
+              if (matchingStones >= 4) {
+                console.log(currentColor + " won!");
+                currentTurn = 0;
+                setCurrentTurn(currentTurn);
+              }
+              matchingStones = 0;
+            }
+          }
+        }
+      }
+
+      // -1: black; 1: white;
+      setCurrentTurn(-currentTurn);
+    }
   }
 
   return <button className={colorClass} onClick={handleClick}></button>;
