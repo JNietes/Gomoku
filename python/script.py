69caed0439820018d43f4cc3623f593ab6c7cc3e
Joaquin Nietes <jnietes04@gmail.com>
2025-11-17 09:50:20 -0500

    Updates

--- python/script.py
+++ python/script.py
@@ -6,6 +6,8 @@ def print_matrix(matrix):
   for row in range(mat.shape[0]):
     string += "["
     for col in range(mat.shape[1]):
-      string += str(mat[row][col]) + ","
-    string += "]"
+      string += " " + str(mat[row][col])
+      if col != mat.shape[1]-1:
+        string += ","
+    string += "]\n"
   return string
