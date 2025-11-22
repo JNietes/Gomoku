39fb05318a0d29e7dc2a71b28419b179db4d48c8
Joaquin Nietes <jnietes04@gmail.com>
2025-11-22 16:36:11 -0500

    Updates

--- python/script.py
+++ python/script.py
@@ -1,5 +1,12 @@
 import numpy as np
 
+# Represents all pieces withing a 4 tile star radius.
+win_matrix = [
+  [[-4, -4], [-3, -3], [-2, -2], [-1, -1], [1, 1], [2, 2], [3, 3], [4, 4]], 
+  [[-4, 4], [-3, 3], [-2, 2], [-1, 1], [1, -1], [2, -2], [3, -3], [4, -4]], 
+  [[-4, 0], [-3, 0], [-2, 0], [-1, 0], [1, 0], [2, 0], [3, 0], [4, 0]],     
+  [[0, -4], [0, -3], [0, -2], [0, -1], [0, 1], [0, 2], [0, 3], [0, 4]]]     
+
 def print_matrix(matrix):
   string = ""
   mat = np.array(matrix)
@@ -11,3 +18,14 @@ def print_matrix(matrix):
         string += ","
     string += "]\n"
   return string
+
+def place_tile(color_int, matrix, row, col):
+  copy = np.array(matrix)
+  if (matrix[row][col] == 0): 
+    copy[row][col] = color_int
+  return copy.tolist()
+
+def detect_winner(matrix):
+  winner = "N/A"
+  return winner
+
